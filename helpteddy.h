3e432a265f25e11227e11bd00b2ee27188b5ad25
torchling <candyrogue@gmail.com>
2016-03-16 17:44:24 +0800

    DS_try2_02

--- helpteddy.h
+++ helpteddy.h
@@ -12,7 +12,14 @@
 bool onTheSameSide(float test_point, float line_start, float line_end, float );
 bool outsideTheTriangle(float test_vertex, float vertex_1, float vertex_2, float vertex_3);
 bool insideTheCircle(float test_vertex, float center_of_circle, float radius);
-void triangle();
-
+vertex centerOfCircumscribedCircle(vertex vertex_1, vertex vertex_2, vertex vertex_3);
+float radiusOfCCircle(vertex testvertex, vertex center);
+bool isBadTriangle(vertex test_point, vertex center, float radius);
+triangle findSuperTriangle ( pointarray );
+float addToEdgelePool( vertex v1 , vertex v2 );
+float addToTrianglePool( vertex v1 , vertex v2 , vertex v3 );
+bool areSameEdges( edge edge1 , edge edge2 );
+void deletDoubleEdge( edgePool );
+void generateDelaunayTruangle( theSetOfInputPoint );
 
 #endif
